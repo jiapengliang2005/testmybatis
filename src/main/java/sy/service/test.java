f7e43e3dafc76b838d7789767544393750d36f4b
12345678 <pljia@isoftstone.com>
2015-08-17 16:35:22 +0800

    第一次上传代码

---
+++ src/main/java/sy/service/test.java
@@ -0,0 +1,11 @@
+package sy.service;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str=String.format("Hi,%s %s", "王力","fdfd");  
+			System.out.println(str);
+	}
+
+}
